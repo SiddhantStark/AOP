a612ecfa67832bafcebd2d9044de0fc481382351
Siddhant Prasad <siddhantprasad@Siddhants-MacBook-Pro.local>
2026-02-17 00:01:14 +0530

    first commit

---
+++ src/test/java/com/example/aopHW/AopHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aopHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
