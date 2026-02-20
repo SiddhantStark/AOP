9b723e79fd180dab2bcbf3c2010d4b77a7215d3b
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2026-02-20 07:55:14 +0530

    Fixes the point cut expression

--- src/main/java/com/example/aopHW/aspects/SecurityLoggingAspect.java
+++ src/main/java/com/example/aopHW/aspects/SecurityLoggingAspect.java
@@ -18,30 +18,36 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 public class SecurityLoggingAspect {
 
-    @Before("execution(* com.example.aop.controller..*(..))")
+    @Before("execution(* com.example.aopHW.controller..*(..))")
     public void logBefore(JoinPoint joinPoint) {
         log.info("Entering method: {} with args {}",
                 joinPoint.getSignature(),
                 joinPoint.getArgs());
     }
 
-    @After("execution(* com.example.aop.controller..*(..))")
+    @After("execution(* com.example.aopHW.controller..*(..))")
     public void logAfter(JoinPoint joinPoint){
         log.info("Executed method: {} with args {}", joinPoint.getSignature(), joinPoint.getArgs());
     }
 
-    @Around("execution(* com.example.aop.controller..*(..))")
+    @Around("execution(* com.example.aopHW..*(..))")
     public Object validateToken(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        log.info("Inside @Around advice for {}", joinPoint.getSignature());
 
         ServletRequestAttributes attributes =
                 (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes == null) {
+            log.error("Request attributes are null");
+            return joinPoint.proceed();
+        }
 
         HttpServletRequest request = attributes.getRequest();
         String token = request.getHeader("Authorization");
 
         if (token == null || !token.equals("Bearer SECRET123")) {
-            log.warn("Invalid or missing token for method: {}",
-                    joinPoint.getSignature());
+            log.warn("Invalid or missing token");
             throw new UnauthorizedException("Invalid Security Token");
         }
 
