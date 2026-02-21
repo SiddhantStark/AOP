6044b61d52f88793f3b0db7487c4784a722f718f
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2026-02-21 15:41:45 +0530

    Add custom annotation for validation for employee entity

--- src/main/java/com/example/aopHW/aspects/GlobalExceptionHandler.java
+++ src/main/java/com/example/aopHW/aspects/GlobalExceptionHandler.java
@@ -5,7 +5,13 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Aspect
@@ -23,6 +29,17 @@ public class GlobalExceptionHandler {
                 joinPoint.getSignature(),
                 joinPoint.getArgs(),
                 ex);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error ->
+                errors.put("error", error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
 
